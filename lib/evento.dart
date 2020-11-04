a407201f6a579ff80c72fb38ab3abe61125691ed
Krasimir Tsonev <krasimir@outset.ws>
2020-11-04 07:40:25 +0200

    Working on the tests

--- lib/evento.dart
+++ lib/evento.dart
@@ -18,13 +18,18 @@ class Evento {
     if (listeners[type] == null) {
       return;
     }
-    listeners[type] = (listeners[type] as List).where((l) => l != listener);
+    listeners[type] =
+        (listeners[type] as List).where((l) => l != listener).toList();
   }
 
-  void dispatch(String type, data) {
+  void dispatch(String type, [data]) {
     if (listeners[type] != null) {
-      listeners[type].forEach((l) => l(data));
+      listeners[type].forEach((l) => l(Event(type: type, data: data)));
     }
+  }
+
+  void reset() {
+    listeners = {};
   }
 }
 
--- test/evento_test.dart
+++ test/evento_test.dart
@@ -3,22 +3,42 @@ import 'package:test/test.dart';
 import '../lib/evento.dart';
 
 void main() {
+  setUp(() {
+    E.reset();
+  });
   test('subscribing to an event', () {
     int counter = 2;
-    E.on('foo', (value) {
-      counter += value;
+    E.on('foo', (event) {
+      counter += event.data;
     });
     E.dispatch('foo', 8);
     expect(counter, 10);
   });
   test('unsubscribing to an event', () {
     int counter = 2;
-    Function handler = (value) {
-      counter += value;
+    Function handler = (event) {
+      counter += event.data;
     };
     E.on('foo', handler);
     E.off('foo', handler);
     E.dispatch('foo', 8);
     expect(counter, 2);
+  });
+  test('support of multiple event types', () {
+    List<String> values = [];
+    Function handlerA = (event) {
+      values.add('a');
+    };
+    Function handlerB = (event) {
+      values.add('b');
+    };
+    E.on('foo', handlerA);
+    E.on('bar', handlerA);
+    E.on('zar', handlerB);
+    E.dispatch('foo');
+    E.dispatch('bar');
+    E.dispatch('zar');
+    E.dispatch('zar');
+    expect(values, ['a', 'a', 'b', 'b']);
   });
 }
